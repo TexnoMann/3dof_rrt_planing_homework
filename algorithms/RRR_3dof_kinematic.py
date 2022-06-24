58df08cbf0f5d588ca91b0d1dc646be4018c69b3
TexnoMann <ElectroDans@yandex.ru>
2022-06-24 18:42:50 +0300

    Add ik solver

---
+++ algorithms/RRR_3dof_kinematic.py
@@ -0,0 +1,71 @@
+import numpy as np
+from numpy.linalg import norm, det, inv
+
+#theta - массив углов, l - массив длин сочленений
+def get_point(theta, l):
+
+  points = np.zeros((3,2))
+
+  points[0, 0] = l[0]*np.cos(theta[0])
+  points[0, 1] = l[0]*np.sin(theta[0])
+  points[1, 0] = points[0, 0] + l[1]*np.cos(theta[0] + theta[1])
+  points[1, 1] = points[0, 1] + l[1]*np.sin(theta[0] + theta[1])
+  points[2, 0] = l[2]*np.cos(theta[0] + theta[1] + theta[2]) + points[1, 0]
+  points[2, 1] = l[2]*np.sin(theta[0] + theta[1] + theta[2]) + points[1, 1]
+
+  return points
+
+def trans_Jacobian_matrix(theta, l):
+
+  f = np.zeros((2,3))
+
+  f[0, 0] = -l[0]*np.sin(theta[0]) - l[1]*np.sin(theta[0] + theta[1]) - l[2]*np.sin(theta[0] + theta[1] + theta[2])
+  f[1, 0] = l[2]*np.cos(theta[0] + theta[1] + theta[2]) + l[1]*np.cos(theta[0] + theta[1]) + l[0]*np.cos(theta[0])
+  f[0, 1] = -l[0]*np.sin(theta[0]) - l[1]*np.sin(theta[0] + theta[1])
+  f[1, 1] = l[1]*np.cos(theta[0] + theta[1]) + l[0]*np.cos(theta[0])
+  f[0, 2] = -l[0]*np.sin(theta[0])
+  f[1, 2] = l[0]*np.cos(theta[0])
+
+  return f.transpose()
+
+def get_angles(theta0, point, l):
+
+  k = 0
+  alpha = 0.2
+  point_init = get_point(theta0, l)[2]
+  e = point - point_init 
+  theta = theta0
+  while norm(e) > 10**(-5):
+    theta = theta0 + alpha*trans_Jacobian_matrix(theta0, l) @ e
+    theta0 = theta
+    point_init = get_point(theta0, l)[2]
+    e = point - point_init
+    k += 1
+    print(k, e)
+    if k>=1e6:
+      return None
+
+  return theta
+
+def get_angles2(theta0, point, l):
+  init_theta = theta0
+  for i in range(0, 50):
+    theta0 = np.random.rand(theta0.shape[0])*np.pi-np.pi/2
+    k = 0
+    alpha = 0.2
+    point_init = get_point(theta0, l)[2]
+    e = point - point_init 
+    theta = theta0
+    while norm(e) > 10**(-3):
+      theta = theta0 + alpha*trans_Jacobian_matrix(theta0, l) @ e
+      theta0 = theta
+      point_init = get_point(theta0, l)[2]
+      e = point - point_init
+      k += 1
+      print(k, e)
+      if k>=5e3:
+        break
+    if norm(e) < 10**(-3):
+      return theta
+  return init_theta
+    
--- tests/tets_3dof_visual.py
+++ tests/tets_3dof_visual.py
@@ -1,12 +1,15 @@
 import pygame
+import time
 import numpy as np
 from typing import Tuple
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from algorithms.collision_checking import Line, Circle, Chain
+from algorithms.RRR_3dof_kinematic import *
 
 SCREEN_SIZE = (800, 800)
 FIELD_SIZE = (3.0, 3.0)
+BASE_POINT = np.array([0.0, 0.0])
 SCALE_FACTOR = np.array(SCREEN_SIZE)/np.array(FIELD_SIZE)
 METRICS_SCALE_FACTOR = np.linalg.norm(np.array(SCREEN_SIZE))/np.linalg.norm(np.array(FIELD_SIZE))
 
@@ -24,6 +27,9 @@ def draw_circle(surf: pygame.display, circle: Circle):
     pygame.draw.circle(surf, (100, 100, 100), circle.p*SCALE_FACTOR, circle.r*METRICS_SCALE_FACTOR)
 
 chain = Chain( (np.array([0.0, 0.0]), np.array([0.3, 0.5]), np.array([0.3, 0.9]), np.array([0.6, 1.6]),) )
+links_length = [1.2,0.9,1.0]
+th = np.array([np.pi/4, np.pi/3, np.pi/4])
+
 c1 = Circle(np.array([0.6, 1.5]), 0.4)
 
 if __name__ == "__main__":
@@ -32,14 +38,29 @@ if __name__ == "__main__":
     screen.fill((255, 255, 255))
 
     running = True
+    time_start = time.time()
     while running:
-
+        screen.fill((255, 255, 255))
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+
+        pos = pygame.mouse.get_pos()
+        pressed1 = pygame.mouse.get_pressed()[0]
+        # Check if rectangle collided with pos and if the left mouse button was pressed
+        if pressed1:
+            print("You have opened a chest!")
+            th = get_angles2(th, pos/SCALE_FACTOR, links_length)
+
+        if th is None:
+            break
+
+        chain = Chain(tuple([BASE_POINT] +list(get_point(th, links_length))))
+
         draw_circle(screen, c1)
         draw_link(screen, chain)
         pygame.display.flip()
+        time.sleep(0.01)
 
     pygame.quit()
